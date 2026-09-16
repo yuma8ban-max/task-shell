82d83e275acbc74b41608d227792722756ffb5a1
yuma8ban-max <yuma8ban@gmail.com>
2026-09-17 05:45:24 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* app-1.0 */
-var CACHE = 'boot-test-v4';
+var CACHE = 'boot-test-v5';
 var FILES = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', function (e) {
